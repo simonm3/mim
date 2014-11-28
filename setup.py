ec3fc0a97aaa3230602ce1a32a8fec529c93761c
simon <simonm3@gmail.com>
2014-11-28 21:13:07 +0000

    minor

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 
 """ simple setup.py with common defaults
+should be usable without editing for all typical projects
 
 Optional configuration files (if missing then assumed blank):
-    requirements.txt => install_requires
+    requirements.txt => installs all dependencies
     README.*         => long_description
     LICENSE.txt      => first line is license
-    .py in root      => scripts
     version.py       => contains __version__
-    data/*.*         => data files
+    .gitignore       => install is same files as git monitorsS
 
 To publish:
-    setup.py register (sends metadata to pypi)
     setup.py sdist (creates tarball of source for uploading to github)
+    git commit -a -m <message>
+    setup.py register (sends metadata to pypi)
+
 
 To install:
     pip install <name>
 """
 from tools.logs import log
 from setuptools import setup, find_packages
+from subprocess import check_output
 import os
 try:
     from version import __version__
@@ -32,7 +35,6 @@ def main():
 
     ###### add bespoke configuration here #######
 
-    setupdict["scripts"].extend(["fwreset", "proxy1"])
 
     #################################################
 
@@ -50,7 +52,9 @@ def defaultSetup():
     github_user = 'simoneva'
     version = __version__
     name = here.split("/")[-1]
+
     setupdict=dict(
+        setup_requires = ["setuptools_git >= 0.3"],
         author       = 'simon',
         author_email = 'simonm3@gmail.com',
         name         = name,
@@ -61,11 +65,8 @@ def defaultSetup():
         url =  'https://pypi.python.org/pypi/{name}/{version}'.format(**locals()),
         download_url = 'https://github.com/{github_user}/{name}/dist/{name}-{version}.tar.gz'
                                 .format(**locals()),
-        data_files = ['data/*.*', 'requirements.txt'],
-        packages     = find_packages(exclude = ['*.pyc', 'contrib*', 'tests*', 'docs*']),
-        scripts = [f for f in os.listdir(here) if os.path.isfile(f)
-                        and f.endswith(".py") and f != "setup.py"],
-        install_requires = install_requires())
+        install_requires = install_requires(),
+        include_package_data=True)
     return setupdict
 
 def install_requires():
@@ -94,6 +95,19 @@ def license():
     except:
         return ""
 
-    dataFiles[name].extend([f for f in os.listdir(here) \
-                            if os.path.isfile(f) and f.find(".") < 0])
 main()
+################### notes on potential changes #################
+""" 
+default using setuptools_git is to include the same files as monitored by github_user
+and ignore the .gitignore
+
+if want to specify files separate to git then....
+
+        data_files = ['data/*.*', 'requirements.txt'],
+        packages     = find_packages(exclude = ['contrib*', 'tests*', 'docs*']),
+        scripts = [f for f in os.listdir(here) if os.path.isfile(f)
+                   and f.endswith(".py") and f != "setup.py"
+                   and f in check_output(["git", "ls-files"])],
+Note data_files MUST include requirements.txt for setup to work
+Note scripts probably would want to only include files in gitfiles
+"""
