b980b919d4ac523da8e1db13963dc954a4eeb6ae
simon <simonm3@gmail.com>
2014-12-25 13:10:08 +0000

    minor

--- setup.py
+++ setup.py
@@ -19,14 +19,13 @@ To publish to pypi and github:
     set __version__ in version.py
     git commit -a -m "message" push
     setup.py register sdist upload => register metadata; tar source; upload to pypi
-
-Note do not use setup.py install or pip install
 """
 from tools.logs import log
 from setuptools import setup, find_packages
 from subprocess import check_output
 import os
 import glob
+import sys
 
 try:
     from version import __version__
@@ -36,6 +35,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 os.chdir(here)
 
 def main():
+    log.info("running setup.py with args=%s"%sys.argv)
     setupdict = defaultSetup()
 
     ###### add application specific configuration here #######
@@ -48,50 +48,46 @@ def main():
           'Programming Language :: Python']
 
     ################################################
-    
-    # log the configuration
-    output=""
-    for k,v in setupdict.items():
-        if k == "long_description":
-            v=v[:25]
-        output+="{k}={v}\n".format(**locals())
-    log.info(output)
-
+    logsetup(setupdict)    
     setup(**setupdict)
 
 def defaultSetup():
     name = here.split("/")[-1]
 
     setupdict=dict(
-        # setuptools_git ensures .tar.gz includes all git tracked files
-        setup_requires = ["setuptools_git >= 0.3"], 
+    	# basic settings to create sdist tar with files under version control
+        setup_requires = ["setuptools_git >= 0.3"],
         author       = 'simon',
         author_email = 'simonm3@gmail.com',
         name         = name,
         version      = __version__,
         description  = name,
         long_description = long_description(),
-        url =  'https://github.com/simonm3/{name}'.format(**locals()),
+        url =  'https://github.com/simonm3/{name}'.format(name=name),
         install_requires = install_requires(),
         
+        # additional settings for pip install
         packages     = find_packages(),
         include_package_data = True,
         scripts = scripts()
-        #data_files = [('plugins', [f for f in glob.glob(os.path.join(here, 'plugins/data/*'))]),
-        #              ('', ['requirements.txt'])],
-        )
+    )
 
     return setupdict
 
+def logsetup(setupdict):
+    """ log the setupdict """
+    output=""
+    for k,v in setupdict.items():
+        if k == "long_description":
+            v=v.splitlines()[0]
+        output+="{k}={v}\n".format(**locals())
+    log.info(output)
+
 def scripts():
-    """ get script files and remove any scripts not managed by git """
-
+    """ get files from scripts folder and remove any scripts not managed by git """
     s = ["scripts/"+f for f in os.listdir('scripts')]
-
     try:
         gitfiles = check_output(["git", "ls-files"]).splitlines()
-        log.info(gitfiles)
-        log.info(s)
         s = [script for script in s if script in gitfiles]
     except:
         # client may not have git installed
@@ -105,7 +101,9 @@ def install_requires():
             reqs = f.read().splitlines()
             return [r.split("#")[0].strip() for r in reqs]
         except:
-        	return []
+            log.exception("problem parsing requirements")
+            sys.exit()
+    return []
 
 def long_description():
     """ set long description = first README.* """
