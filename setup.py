ed01306a699f2f3ad0997aae7e82173a76539cf1
simon <simonm3@gmail.com>
2014-12-30 19:15:09 +0000

    split setup and setup.cfg

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@
 """
 simple setup.py with common defaults
 
-Configuration (all optional):
+Configuration files (all optional):
     requirements.txt => list of dependencies to be installed
     README.*         => first found is long_description
     LICENSE.txt      => license
     version.py       => contains __version__
     .gitignore       => files to exclude from git and installation
     everything in the scripts folder is treated as a script
-
-No other configuration files are needed
+    setup.cfg        => metadata section for setup. adds further data and overrides the above
 
 To publish to pypi and github:
     set __version__ in version.py
     git commit -a -m "message" push
     setup.py register sdist upload => register metadata; tar source; upload to pypi
 """
+import ConfigParser
 from tools.logs import log
 from setuptools import setup, find_packages
 from subprocess import check_output
@@ -32,47 +32,41 @@ here = os.path.abspath(os.path.dirname(__file__))
 os.chdir(here)
 
 def main():
-    log.info("***** running setup.py with args=%s"%sys.argv)
+    log.info("***** running setup.py with args=%s *****"%sys.argv)
+
     setupdict = defaultSetup()
-
-    ###### add application specific configuration here #######
-
-    setupdict["classifiers"]= [ \
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python']
-
-    ################################################
+    setupdict.update(cfgSetup())
     logsetup(setupdict)    
     setup(**setupdict)
 
 def defaultSetup():
+    """ returns default dict """
     name = here.split("/")[-1]
-
-    install_requires, dependency_links = getRequirements()
-
     setupdict=dict(
     	# basic settings to create sdist tar with files under version control
+        name = name,
+        description = name,
         setup_requires = ["setuptools_git >= 0.3"],
-        author       = 'simon',
-        author_email = 'simonm3@gmail.com',
-        name         = name,
         version      = __version__,
-        description  = name,
         long_description = long_description(),
-        url =  'https://github.com/simonm3/{name}'.format(name=name),
-        install_requires = install_requires,
-        dependency_links = dependency_links,
+        install_requires = install_requires(),
+        dependency_links = dependency_links(),
         
         # additional settings for pip install
         packages     = find_packages(),
         include_package_data = True,
         scripts = scripts()
     )
-
     return setupdict
+
+def cfgSetup():
+    """ returns setup.cfg dict """
+    c = ConfigParser.ConfigParser(allow_no_value=True)
+    try:
+        c.readfp(open("setup.cfg"))
+        return dict(c.items("metadata"))
+    except:
+        return dict()
 
 def logsetup(setupdict):
     """ log the setupdict """
@@ -86,26 +80,37 @@ def logsetup(setupdict):
 def scripts():
     """ get files from scripts folder and remove any scripts not managed by git """
     s = ["scripts/"+f for f in os.listdir('scripts')]
-    try:
+    if sys.argv[1] == "sdist":
         gitfiles = check_output(["git", "ls-files"]).splitlines()
-        s = [script for script in s if script in gitfiles]
-    except:
-        # client may not have git installed
-        pass
+        # must remove else any scripts specified are included even if not managed by git
+        #s = [script for script in s if script in gitfiles]
     return s
 
-def getRequirements():
+def install_requires():
     """ use requirements.txt """
-    with open("requirements.txt", "r") as f:
-        try:
+    try:
+        with open("requirements.txt", "r") as f:
+            reqs = [req.split("#")[0].strip() for req in f.read().splitlines()]
+            install_requires = [req for req in reqs if not req.startswith("http")]
+            return install_requires
+    except IOError:
+        return []
+    except:
+        log.exception("problem parsing install_requires")
+        sys.exit()
+
+def dependency_links():
+    """ use requirements.txt """
+    try:
+        with open("requirements.txt", "r") as f:
             reqs = [req.split("#")[0].strip() for req in f.read().splitlines()]
             dependency_links = [req for req in reqs if req.startswith("http")]
-            install_requires = [req for req in reqs if not req.startswith("http")]
-            return install_requires, dependency_links
-        except:
-            log.exception("problem parsing requirements")
-            sys.exit()
-    return []
+            return dependency_links
+    except IOError:
+        return []
+    except:
+        log.exception("problem parsing dependency_links")
+        sys.exit()
 
 def long_description():
     """ set long description = first README.* """
